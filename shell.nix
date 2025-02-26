725e75bb08841ad054eb979ab93a7ea468d0a5da
Pawel Lisewski <dev@palekiwi.com>
2025-02-26 19:47:25 +0800

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,17 @@
 { pkgs, ... }:
 
 pkgs.mkShell {
-  name = "rspec.nvim";
+  name = "rspec-runner";
   buildInputs =
     with pkgs; [
       luajit
       luajitPackages.busted
       luajitPackages.nlua
       luajitPackages.nui-nvim
+
+      # (vimPlugins.nvim-treesitter.withPlugins (p: [ p.ruby ]))
+      vimPlugins.nvim-treesitter
+      vimPlugins.nvim-treesitter-parsers.ruby
 
       ruby_3_4
       rubyPackages_3_4.rspec
