6239182f9c76c6048f03e30e296d7c11d9a4e242
Pawel Lisewski <dev@palekiwi.com>
2025-03-05 17:17:44 +0800

    Create autocommands

--- lua/rspec-runner/init.lua
+++ lua/rspec-runner/init.lua
@@ -6,11 +6,17 @@ local M = {
   state = require("rspec-runner.state").new()
 }
 
+---@class UserConfig
+---@field cmd string[]
+
 ---@param cfg table
 function M.setup(cfg)
   if cfg ~= nil then
     M.config = vim.tbl_deep_extend("force", M.config, cfg)
   end
+
+  vim.api.nvim_command "command! RspecRunnerFile lua require('rspec-runner').run('file')<CR>"
+  vim.api.nvim_command "command! RspecRunnerAll lua require('rspec-runner').run('all')<CR>"
 end
 
 ---@param scope Runner.Scope
@@ -21,6 +27,5 @@ function M.run(scope)
   return executor.execute(runner, M.config, M.state)
 end
 
-vim.keymap.set("n", "<leader>rn", function() M.run("file") end)
 
 return M
